655e68414c1b75342150e105456fc1a6a956787c
Chloe Lim <chloelim22@gmail.com>
2024-09-19 05:57:21 +0800

    feat: set up shadcn

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,9 +21,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body
