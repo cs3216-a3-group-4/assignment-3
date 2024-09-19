bf2fd4502f5e55f817f003c658140471ed76db09
Chloe Lim <chloelim22@gmail.com>
2024-09-20 00:59:48 +0800

    feat: change font to inter
    goodbye geist

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter as FontSans } from "next/font/google";
 
 import Navbar from "@/components/navigation/navbar";
+import { cn } from "@/lib/utils";
 
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
