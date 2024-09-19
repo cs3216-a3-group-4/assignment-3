1ef60f5e4ea66c9fa1c3d4581d61629c4146ec74
Chloe Lim <chloelim22@gmail.com>
2024-09-20 01:01:49 +0800

    refactor: remove site config
    store name as env var instead

--- frontend/components/navigation/navbar.tsx
+++ frontend/components/navigation/navbar.tsx
@@ -6,10 +6,8 @@ import {
   NavigationMenuList,
 } from "@radix-ui/react-navigation-menu";
 
-import { siteConfig } from "@/config/site";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import { NavItem } from "@/types/navigation";
-
-import { navigationMenuTriggerStyle } from "../ui/navigation-menu";
 
 export const NavItems: NavItem[] = [];
 
@@ -18,7 +16,9 @@ function Navbar() {
     <header className="w-full sticky top-0 bg-background z-50 backdrop-opacity-80">
       <div className="container flex items-center justify-between px-8 py-4 md:px-20">
         <Link className="mr-6 flex items-center gap-x-2" href="/">
-          <span className="inline-block font-bold">{siteConfig.name}</span>
+          <span className="inline-block font-bold">
+            {process.env.NEXT_PUBLIC_APP_NAME}
+          </span>
         </Link>
         <NavigationMenu>
           <NavigationMenuList>
--- frontend/config/site.ts
+++
@@ -1,7 +0,0 @@
-export const siteConfig = {
-  // TODO: change these
-  name: "Group 4",
-  description: "CS3216 Assignment 3 (Group 4) - placeholder",
-};
-
-export type SiteConfig = typeof siteConfig;
