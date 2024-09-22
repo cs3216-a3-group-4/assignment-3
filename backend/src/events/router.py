916ca3b612b36ce4e3b1954ce0ae15b7e6740fbb
seeleng <seelengxd@gmail.com>
2024-09-23 00:28:45 +0800

    feat: add date filter to /events

--- backend/src/events/router.py
+++ backend/src/events/router.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from typing import Annotated
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 from src.auth.dependencies import get_current_user
 from src.auth.models import User
-from src.events.models import Category, Event
+from src.events.models import Article, Category, Event
 from src.common.dependencies import get_session
 from src.events.schemas import EventDTO, EventIndexResponse
 
@@ -15,6 +16,8 @@ router = APIRouter(prefix="/events", tags=["events"])
 @router.get("/")
 def get_events(
     _: Annotated[User, Depends(get_current_user)],
+    start_date: Annotated[datetime | None, Query()] = None,
+    end_date: Annotated[datetime | None, Query()] = None,
     session=Depends(get_session),
     category_ids: Annotated[list[int] | None, Query()] = None,
     limit: int | None = None,
@@ -35,6 +38,15 @@ def get_events(
         event_query = event_query.limit(limit)
     if offset is not None:
         event_query = event_query.offset(offset)
+    if start_date is not None:
+        event_query = event_query.where(
+            Event.original_article.has(Article.date >= start_date)
+        )
+    if end_date is not None:
+        event_query = event_query.where(
+            Event.original_article.has(Article.date <= end_date)
+        )
+    event_query = event_query.order_by(Event.rating.desc(), Event.date.desc())
 
     events = list(session.scalars(event_query))
     return EventIndexResponse(total_count=total_count, count=len(events), data=events)
