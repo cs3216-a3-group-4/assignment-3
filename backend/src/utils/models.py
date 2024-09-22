0da48aaac424a2f0e8e28d40dad20d123f61a955
seeleng <seelengxd@gmail.com>
2024-09-22 21:36:25 +0800

    feat: add userquestion models

--- backend/src/utils/models.py
+++ backend/src/utils/models.py
@@ -2,3 +2,4 @@
 
 from src.auth import models as auth_models  # noqa: F401
 from src.events import models as event_models  # noqa: F401
+from src.user_questions import models as user_question_models  # noqa: F401
