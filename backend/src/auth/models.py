0b4dcb16267f4d3319bccbc5575ecd03883fbb15
seeleng <seelengxd@gmail.com>
2024-09-22 11:48:58 +0800

    feat(models): add categories to user

--- backend/src/auth/models.py
+++ backend/src/auth/models.py
@@ -1,11 +1,21 @@
 from enum import Enum
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.common.base import Base
+from src.events.models import Category
 
 
 class AccountType(str, Enum):
     NORMAL = "normal"
     GOOGLE = "google"
+
+
+user_category_table = Table(
+    "user_category",
+    Base.metadata,
+    Column("user_id", ForeignKey("user.id")),
+    Column("category_id", ForeignKey("category.id")),
+)
 
 
 class User(Base):
@@ -15,3 +25,5 @@ class User(Base):
     email: Mapped[str]
     hashed_password: Mapped[str]
     account_type: Mapped[AccountType]
+
+    categories: Mapped[list[Category]] = relationship(secondary=user_category_table)
