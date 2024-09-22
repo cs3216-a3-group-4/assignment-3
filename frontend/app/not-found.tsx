4db2a14a64b0cb7a3944c6ff71cf88db83e6a923
Chloe Lim <chloelim22@gmail.com>
2024-09-23 01:28:43 +0800

    feat: add 404 page

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,29 @@
+import JippyClownIcon from "@/assets/jippy-clown";
+import { Button } from "@/components/ui/button";
+
+function AboutPage() {
+  return (
+    <div className="flex flex-col space-y-4 m-auto px-12 md:px-24">
+      <div className="flex flex-col space-y-8">
+        <JippyClownIcon />
+        <div className="flex flex-col space-y-2">
+          <h3 className="text-lg md:text-2xl font-medium font-mono">
+            404 Not Found Error
+          </h3>
+          <h1 className="text-3xl md:text-5xl font-semibold">
+            Ribbit! Jippy couldn&apos;t find that page...
+          </h1>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-8">
+        <p>
+          The page you were looking for doesn&apos;t exist. The page might have
+          moved, or you might have clicked on a bad link.
+        </p>
+        <Button size="lg">Bring me back to my home page</Button>
+      </div>
+    </div>
+  );
+}
+
+export default AboutPage;
