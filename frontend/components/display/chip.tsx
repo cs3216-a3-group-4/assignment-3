58c921b9b30d745e5049583ea54da2aa89472fff
Chloe Lim <chloelim22@gmail.com>
2024-09-24 04:31:16 +0800

    feat: add news article

--- frontend/components/display/chip.tsx
+++ frontend/components/display/chip.tsx
@@ -2,6 +2,7 @@ import { cva, VariantProps } from "class-variance-authority";
 
 import { Box } from "@/components/ui/box";
 import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
 
 const chipVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-xs font-medium transition-colors disabled:pointer-events-none disabled:opacity-50",
@@ -10,6 +11,8 @@ const chipVariants = cva(
       variant: {
         default: "bg-sky-200/60 text-sky-600 hover:bg-sky-200/40",
         destructive: "bg-red-200/60 text-destructive/90 hover:bg-red-200/40",
+        green: "bg-[#C5EBD9]/60 text-[#2D835A] hover:bg-[#C5EBD9]/40",
+        greygreen: "bg-[#D2DAD6]/60 text-[#4C6559] hover:bg-[#D2DAD6]/40",
       },
       size: {
         default: "h-6 px-2 py-2",
@@ -26,12 +29,14 @@ const chipVariants = cva(
 
 interface ChipProps extends VariantProps<typeof chipVariants> {
   label: string;
-  className: string;
+  className?: string;
+  Icon?: LucideIcon;
 }
 
-const Chip = ({ label, variant, size, className }: ChipProps) => {
+const Chip = ({ label, variant, size, className, Icon }: ChipProps) => {
   return (
     <Box className={cn(chipVariants({ variant, size }), className)}>
+      {Icon && <Icon size={16} strokeWidth={1.8} className="mr-2" />}
       {label}
     </Box>
   );
---
+++ frontend/components/news/news-article.tsx
@@ -0,0 +1,57 @@
+import {
+  categoriesToDisplayName,
+  categoriesToIconsMap,
+  Category,
+} from "@/types/categories";
+import { ArrowUpRightIcon } from "lucide-react";
+import Chip from "../display/chip";
+import Image from "next/image";
+
+const sampleArticleCategories = [
+  Category.Economics,
+  Category.Environment,
+  Category.Media,
+  Category.Politics,
+];
+
+const NewsArticle = () => {
+  return (
+    <div className="flex w-full border-y-[1px] py-10 px-6 space-x-16">
+      <div className="flex flex-col w-7/12">
+        <div className="flex w-full justify-between text-sm text-offblack">
+          <span>
+            <ArrowUpRightIcon size={16} className="inline-flex" /> CNA, Guardian
+          </span>
+          <span>21 Sep 2024</span>
+        </div>
+        <h2 className="text-lg font-semibold mt-2">
+          Norris Claims Singapore GP Pole Amid Ferrari’s Setback
+        </h2>
+        <p className="text-sm text-offblack">
+          A Reflection on Commercialization, Sustainability, and Global Sports
+          as Cultural Forces
+        </p>
+        <div className="flex space-x-2 mt-6">
+          {sampleArticleCategories.map((category) => (
+            <Chip
+              label={categoriesToDisplayName[category]}
+              Icon={categoriesToIconsMap[category]}
+              variant="greygreen"
+            />
+          ))}
+        </div>
+      </div>
+      <div className="flex w-5/12">
+        <Image
+          width={273}
+          height={154}
+          unoptimized
+          src="https://onecms-res.cloudinary.com/image/upload/s--893X2dru--/c_fill,g_auto,h_468,w_830/fl_relative,g_south_east,l_mediacorp:cna:watermark:2021-08:cna,w_0.1/f_auto,q_auto/v1/mediacorp/cna/image/2024/09/21/RAY_3553.JPG?itok=aFJ8bcqO"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default NewsArticle;
