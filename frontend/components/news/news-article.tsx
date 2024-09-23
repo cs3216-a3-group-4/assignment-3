7050b6444ef77e7f69de1c978e7e869c97b69e59
Chloe Lim <chloelim22@gmail.com>
2024-09-24 04:31:43 +0800

    feat: add mobile responsiveness for news article

--- frontend/components/news/news-article.tsx
+++ frontend/components/news/news-article.tsx
@@ -16,8 +16,8 @@ const sampleArticleCategories = [
 
 const NewsArticle = () => {
   return (
-    <div className="flex w-full border-y-[1px] py-10 px-6 space-x-16">
-      <div className="flex flex-col w-7/12">
+    <div className="flex flex-col-reverse lg:flex-row w-full border-y-[1px] py-10 px-6 gap-x-16">
+      <div className="flex flex-col w-full lg:w-7/12">
         <div className="flex w-full justify-between text-sm text-offblack">
           <span>
             <ArrowUpRightIcon size={16} className="inline-flex" /> CNA, Guardian
@@ -31,7 +31,7 @@ const NewsArticle = () => {
           A Reflection on Commercialization, Sustainability, and Global Sports
           as Cultural Forces
         </p>
-        <div className="flex space-x-2 mt-6">
+        <div className="flex flex-wrap gap-x-2 gap-y-2 mt-6">
           {sampleArticleCategories.map((category) => (
             <Chip
               label={categoriesToDisplayName[category]}
@@ -41,13 +41,17 @@ const NewsArticle = () => {
           ))}
         </div>
       </div>
-      <div className="flex w-5/12">
+      <div className="flex w-full lg:w-5/12 mb-6 items-center">
         <Image
           width={273}
           height={154}
           unoptimized
           src="https://onecms-res.cloudinary.com/image/upload/s--893X2dru--/c_fill,g_auto,h_468,w_830/fl_relative,g_south_east,l_mediacorp:cna:watermark:2021-08:cna,w_0.1/f_auto,q_auto/v1/mediacorp/cna/image/2024/09/21/RAY_3553.JPG?itok=aFJ8bcqO"
           alt=""
+          style={{
+            width: "100%",
+            height: "fit-content",
+          }}
         />
       </div>
     </div>
