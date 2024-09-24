f47bae0a01306f6954dcba37c2cba2f8cd80ecf0
marcus-ny <kyalsin321@gmail.com>
2024-09-24 12:03:10 +0800

    refactor: file org

--- backend/src/lm/generate_events.py
+++ backend/src/lm/generate_events.py
@@ -8,6 +8,7 @@ from src.scrapers.guardian.scrape import query_page
 from src.common.constants import LANGCHAIN_API_KEY
 from src.common.constants import LANGCHAIN_TRACING_V2
 from src.common.constants import OPENAI_API_KEY
+from src.lm.prompts import SYSPROMPT
 
 import os
 
@@ -49,54 +50,6 @@ class Example(BaseModel):
 class EventDetails(BaseModel):
     examples: list[Example]
 
-
-SYSPROMPT = """
-    You are a Singaporean student studying for your A Levels. You are curating examples to supplement and bolster your arguments in your General Paper essays.
-    Given an article, you should provide relevant examples that can be used to support or refute arguments in a General Paper essay.
-    Given the article, you should also generate 2-3 GCE A Level General Paper essay questions that can potentially be answered using the events you have provided.
-    You should provide at most 3 example events. If there are no relevant events, you should provide an empty list. You do not have to always provide 3 examples.
-
-    For each event, your title should be specific enough so that I can guess your example without looking at the paragraph.
-    For each event, you should categorize your examples into the following categories:
-    [Arts & Humanities, Science & Tech, Politics, Media, Environment, Education, Sports, Gender & Equality, Religion, Society & Culture, Economic]
-    Important Note: Only categorize an event if the connection to a category is direct, significant, and not merely tangential. Do NOT categorize an event if the connection is speculative or minor.
-
-    For each event, you should give an analysis of how the event can be used in the context of its respective category i.e. the categories that you have chosen for this event.
-    The analysis should be specific to the category of the event and should be tailored to the context of General Paper essays. Provide coherent arguments and insights. Be sure to give a detailed analysis of 3-4 sentences.
-    In your analysis, you should not mention "General Paper" or "A Levels".
-    For the analysis, remember that this is in the context of General Paper which emphasises critical thinking and the ability to construct coherent arguments.
-    If needed, you can think about the questions you have generated and how the event can be used to write about points for/against the argument in the question.
-
-    For each event, you should also indicate if the event happened in Singapore. If there is no indication in the article, you should assume that the event did not happen in Singapore.
-    For each event, you should also give a rating from 1 to 5 on how useful the event is as an example for a General Paper essay.
-    You should give me your examples in the following json format:
-
-    { 
-    "examples": [
-        { 
-        "event_title": "Title of the event",
-        "description": "The example that supports or refutes the argument",
-        "questions": ["Question 1", "Question 2", "Question 3"],
-        "category": "Array of categories for this event. For example ['Arts & Humanities', 'Science & Tech'], 
-        "analysis_list": [
-            {
-            "category": "The category of the event for example 'Arts & Humanities'",
-            "analysis": "The analysis of how the example can be used in a General Paper essay for Arts & Humanities"
-            },
-            {
-            "category": "The category of the event for example 'Science & Tech'",
-            "analysis": "The analysis of how the example can be used in a General Paper essay for Science & Tech"
-            }
-        ],
-        "in_singapore": "Boolean value to indicate if the event happened in Singapore",
-        "rating": "The rating of how useful the event is as an example for a General Paper essay"
-        }
-        ]
-    }
-
-    The article:
-
-"""
 
 sample_text = """
 A seething Mikel Arteta admitted that he was “amazed about how inconsistent the decisions can be” after Declan Rice was sent off as Arsenal dropped their first points of the new season against Brighton.
@@ -177,40 +130,6 @@ def form_event_json(event_details) -> dict:
 """
 Generate a batch of prompts for OpenAI Batch API to generate events from articles
 """
-
-
-def generate_batch_prompts() -> List[str]:
-    articles = get_articles()
-    batch_prompts = []
-    id = 0
-    for article in articles:
-        request_id = f"request_{id}"
-        id += 1
-        batch_prompts.append(
-            {
-                "custom_id": request_id,
-                "method": "POST",
-                "url": "/v1/chat/completions",
-                "body": {
-                    "model": "gpt-4o-mini",
-                    "messages": [
-                        {"role": "system", "content": SYSPROMPT},
-                        {
-                            "role": "user",
-                            "content": article.get("fields").get("bodyText"),
-                        },
-                    ],
-                    # "metadata": {
-                    #     "article_id": f"article_{id}",
-                    # },
-                    "max_tokens": 1000,
-                },
-            }
-        )
-
-    with open("batch_prompts.jsonl", "w") as jsonl_file:
-        for item in batch_prompts:
-            jsonl_file.write(json.dumps(item) + "\n")
 
 
 def generate_events_from_article(article: str) -> dict:
