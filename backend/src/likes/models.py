07313e4823ff70e6617e9b5adcaa9a5c8fe48f10
seeleng <seelengxd@gmail.com>
2024-09-26 00:24:19 +0800

    feat: add likes table

---
+++ backend/src/likes/models.py
@@ -0,0 +1,23 @@
+from enum import Enum
+from sqlalchemy import ForeignKey
+from src.common.base import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+class LikeType(int, Enum):
+    LIKE = 1
+    DISLIKE = -1
+
+
+class Like(Base):
+    __tablename__ = "like"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    point_id: Mapped[int] = mapped_column(ForeignKey("point.id"), nullable=True)
+    analysis_id: Mapped[int] = mapped_column(ForeignKey("analysis.id"))
+    type: Mapped[LikeType]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    # relationships
+    point = relationship("Point", backref="likes")
+    analysis = relationship("Analysis", backref="likes")
+    user = relationship("User", backref="likes")
