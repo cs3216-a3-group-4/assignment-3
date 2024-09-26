b4922d87d61d2d5f5034e0ea6f64ab97d8e38ac7
seeleng <seelengxd@gmail.com>
2024-09-27 03:56:03 +0800

    feat: paginate categories page

--- frontend/app/(authenticated)/categories/[id]/page.tsx
+++ frontend/app/(authenticated)/categories/[id]/page.tsx
@@ -1,19 +1,41 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 
-import { CategoryDTO, MiniEventDTO } from "@/client";
-import ArticleLoading from "@/components/news/article-loading";
-import NewsArticle from "@/components/news/news-article";
+import { CategoryDTO } from "@/client";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import { getCategories } from "@/queries/category";
 import { getEventsForCategory } from "@/queries/event";
 
+import Articles from "./articles-list";
+
+function isNumeric(value: string | null) {
+  return value !== null && /^-?\d+$/.test(value);
+}
+
 const Page = ({ params }: { params: { id: string } }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   const categoryId = parseInt(params.id);
+
+  const pageStr = searchParams.get("page");
+
+  const page = isNumeric(pageStr) ? parseInt(pageStr!) : 1;
+
   const [categoryName, setCategoryName] = useState<string>("");
   const { data: events, isSuccess: isEventsLoaded } = useQuery(
-    getEventsForCategory(categoryId),
+    getEventsForCategory(categoryId, page),
   );
   const { data: categories, isSuccess: isCategoriesLoaded } =
     useQuery(getCategories());
@@ -29,32 +51,40 @@ const Page = ({ params }: { params: { id: string } }) => {
     }
   }, [categories, isCategoriesLoaded, categoryId]);
 
-  const Articles = () => {
-    if (!isEventsLoaded) {
-      return (
-        <>
-          <ArticleLoading />
-          <ArticleLoading />
-          <ArticleLoading />
-        </>
-      );
-    }
+  const getPageUrl = (page: number) => {
+    // now you got a read/write object
+    const current = new URLSearchParams(Array.from(searchParams.entries())); // -> has to use this form
 
-    const eventData = events!.data;
-    if (eventData.length == 0) {
-      return (
-        <div className="flex w-full justify-center">
-          <p className="text-sm text-offblack">
-            No recent events. Try refreshing the page.
-          </p>
-        </div>
-      );
-    }
+    // update as necessary
+    current.set("page", page.toString());
 
-    return eventData.map((newsEvent: MiniEventDTO, index: number) => (
-      <NewsArticle key={index} newsEvent={newsEvent} />
-    ));
+    // cast to string
+    const search = current.toString();
+    // or const query = `${'?'.repeat(search.length && 1)}${search}`;
+    const query = search ? `?${search}` : "";
+
+    return `${pathname}${query}`;
   };
+
+  const changePage = (page: number) => {
+    router.push(getPageUrl(page));
+  };
+
+  if (!pageStr) {
+    changePage(1);
+    return;
+  }
+
+  const items = events?.total_count;
+  const pageCount = items !== undefined && Math.ceil(items / 10);
+  if (pageCount !== false) {
+    if (page <= 0) {
+      changePage(1);
+    }
+    if (page > pageCount) {
+      changePage(pageCount);
+    }
+  }
 
   return (
     <div className="flex flex-col w-full py-8">
@@ -68,8 +98,60 @@ const Page = ({ params }: { params: { id: string } }) => {
       </div>
 
       <div className="flex flex-col w-auto mx-4 md:mx-8 xl:mx-24">
-        <Articles />
+        <Articles eventData={events?.data} isEventsLoaded={isEventsLoaded} />
       </div>
+
+      {isEventsLoaded && (
+        <Pagination className="py-8">
+          <PaginationContent>
+            {page !== 1 && (
+              <PaginationItem>
+                <PaginationPrevious href={getPageUrl(page - 1)} />
+              </PaginationItem>
+            )}
+            {/* Only for last page */}
+            {page == pageCount && (
+              <PaginationItem>
+                <PaginationLink href={getPageUrl(page - 2)}>
+                  {page - 2}
+                </PaginationLink>
+              </PaginationItem>
+            )}
+            {page !== 1 && (
+              <PaginationItem>
+                <PaginationLink href={getPageUrl(page - 1)}>
+                  {page - 1}
+                </PaginationLink>
+              </PaginationItem>
+            )}
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                {page}
+              </PaginationLink>
+            </PaginationItem>
+            {page !== pageCount && (
+              <PaginationItem>
+                <PaginationLink href={getPageUrl(page + 1)}>
+                  {page + 1}
+                </PaginationLink>
+              </PaginationItem>
+            )}
+            {/* Only for first page */}
+            {page === 1 && (pageCount as number) >= 3 && (
+              <PaginationItem>
+                <PaginationLink href={getPageUrl(page + 2)}>
+                  {page + 2}
+                </PaginationLink>
+              </PaginationItem>
+            )}
+            {page !== pageCount && (
+              <PaginationItem>
+                <PaginationNext href={getPageUrl(page + 1)} />
+              </PaginationItem>
+            )}
+          </PaginationContent>
+        </Pagination>
+      )}
     </div>
   );
 };
