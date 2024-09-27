70ebbdaab58f7bbb9c98806f83f95c04d547f114
Chloe Lim <chloelim22@gmail.com>
2024-09-27 18:32:54 +0800

    feat: answer page ui changes

--- frontend/app/(authenticated)/answers/[id]/page.tsx
+++ frontend/app/(authenticated)/answers/[id]/page.tsx
@@ -7,13 +7,14 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { BookOpenTextIcon, SparkleIcon, ZapIcon } from "lucide-react";
 
 const AnswerPage = () => {
   const mockData: UserQuestionMiniDTO = retryResp;
 
   return (
     <div className="flex flex-col bg-muted w-full h-full max-h-full px-4 md:px-8 xl:px-24 overflow-y-auto">
-      <div className="flex flex-col pb-4 mb-4 py-8 xl:py-16 max-w-5xl md:mx-8 lg:mx-16 xl:mx-auto">
+      <div className="flex flex-col pb-4 mb-4 py-8 xl:py-16 max-w-6xl md:mx-8 lg:mx-16 xl:mx-auto">
         <h1 className="px-8 md:px-0 text-2xl lg:text-3x xl:text-4xl font-semibold text-text mb-10 2xl:mb-12">
           {mockData.question}
         </h1>
@@ -35,7 +36,7 @@ const AnswerPage = () => {
                     <div className="flex flex-col">
                       <Chip
                         // TODO @seeleng: my tag not centered pls fix thanku :clown:
-                        className="inline-block mb-4 w-fit max-w-full"
+                        className="inline-block mb-4 w-fit max-w-full 2xl:text-xl"
                         label={point.positive ? "For" : "Against"}
                         size="lg"
                         variant={point.positive ? "secondary" : "accent"}
@@ -47,34 +48,63 @@ const AnswerPage = () => {
                   </AccordionTrigger>
                   <AccordionContent>
                     <div className="border-t-[0.5px] border-primary-600 mt-2 pt-4 2xl:pt-8">
-                      <h2 className="text-lg 2xl:text-2xl px-6 2xl:px-10 pb-4 2xl:pb-0 pt-2">
-                        Examples
-                      </h2>
+                      <span className="flex items-center text-lg 2xl:text-2xl px-6 2xl:px-10 pb-2 2xl:pb-0 pt-2">
+                        <BookOpenTextIcon
+                          className="inline-flex mr-3"
+                          size={20}
+                          strokeWidth={1.6}
+                        />
+                        <h2>Jippy Examples</h2>
+                      </span>
 
                       {pointHasAnalysis ? (
                         <Accordion className="" type="multiple">
                           {/* TODO: get confidence score, sort and bucket */}
-                          {point.point_analysises.map((point_analysis) => {
-                            const { analysis, elaboration } = point_analysis;
-                            const { id: analysisId, event } = analysis;
-                            return (
-                              <AccordionItem
-                                className="py-2 2xl:py-4 px-6 2xl:px-10"
-                                value={analysisId.toString()}
-                                key={analysisId}
-                              >
-                                <AccordionTrigger
-                                  className="text-start text-lg xl:text-xl 3xl:text-2xl text-primary-alt-800 font-medium"
-                                  chevronClassName="ml-4"
+                          {point.point_analysises.map(
+                            (point_analysis, index) => {
+                              const { analysis, elaboration } = point_analysis;
+                              const { id: analysisId, event } = analysis;
+                              return (
+                                <AccordionItem
+                                  className="py-2 2xl:py-4 px-6 2xl:px-10"
+                                  value={analysisId.toString()}
+                                  key={analysisId}
                                 >
-                                  {event.title}
-                                </AccordionTrigger>
-                                <AccordionContent className="text-lg 2xl:text-xl 3xl:text-2xl">
-                                  <div>{elaboration}</div>
-                                </AccordionContent>
-                              </AccordionItem>
-                            );
-                          })}
+                                  <AccordionTrigger
+                                    className="text-start text-lg xl:text-xl 3xl:text-2xl text-primary-alt-800 font-medium"
+                                    chevronClassName="ml-4"
+                                  >
+                                    {index + 1}. {event.title}
+                                  </AccordionTrigger>
+                                  <AccordionContent className="flex flex-col gap-y-8 2xl:gap-y-10 text-lg 2xl:text-xl 3xl:text-2xl py-2 2xl:py-4">
+                                    <div className="flex flex-col gap-y-4">
+                                      <span className="font-medium text-lg 2xl:text-xl text-text-muted">
+                                        <ZapIcon
+                                          className="inline-flex mr-3"
+                                          size={20}
+                                        />
+                                        Event summary
+                                      </span>
+                                      <blockquote className="border-l-2 pl-6 italic text-gray-800">
+                                        {event.description}
+                                      </blockquote>
+                                    </div>
+
+                                    <div className="flex flex-col gap-y-4">
+                                      <span className="font-medium text-lg 2xl:text-xl text-text-muted">
+                                        <SparkleIcon
+                                          className="inline-flex mr-3"
+                                          size={20}
+                                        />
+                                        Possible argument
+                                      </span>
+                                      <p>{elaboration}</p>
+                                    </div>
+                                  </AccordionContent>
+                                </AccordionItem>
+                              );
+                            },
+                          )}
                         </Accordion>
                       ) : (
                         <div>
