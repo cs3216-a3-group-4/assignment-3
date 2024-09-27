1e452997200123b0e4bd648762de9043ad7a262b
Chloe Lim <chloelim22@gmail.com>
2024-09-27 17:01:55 +0800

    feat: finish up base answers

--- frontend/app/(authenticated)/answers/[id]/page.tsx
+++ frontend/app/(authenticated)/answers/[id]/page.tsx
@@ -10,6 +10,7 @@ import {
 
 const AnswerPage = () => {
   const mockData: UserQuestionMiniDTO = retryResp;
+
   return (
     <div className="flex flex-col bg-muted w-full h-full max-h-full px-4 md:px-8 xl:px-24 overflow-y-auto">
       <div className="flex flex-col pb-4 mb-4 py-8 xl:py-16 max-w-5xl md:mx-8 lg:mx-16 xl:mx-auto">
@@ -19,62 +20,84 @@ const AnswerPage = () => {
         <div className="flex flex-col">
           <Accordion type="multiple" className="flex flex-col gap-y-4">
             {/* TODO @seeleng: sort by position? for arguments first? */}
-            {mockData.answer.points.map((point) => (
-              <AccordionItem
-                className="border border-primary/15 rounded-lg px-8 py-2 bg-background"
-                key={point.id}
-                value={point.id.toString()}
-              >
-                <AccordionTrigger
-                  chevronClassName="h-6 w-6 stroke-[2.5]"
-                  className="text-lg lg:text-xl 2xl:text-2xl text-primary font-medium text-start hover:no-underline"
+            {mockData.answer.points.map((point) => {
+              const pointHasAnalysis = point.point_analysises.length > 0;
+              return (
+                <AccordionItem
+                  className="border border-primary/15 rounded-lg px-8 py-2 2xl:px-12 2xl:py-6 bg-background"
+                  key={point.id}
+                  value={point.id.toString()}
                 >
-                  <div className="flex flex-col">
-                    <Chip
-                      // TODO @seeleng: my tag not centered pls fix thanku :clown:
-                      className="inline-block mb-4 w-fit max-w-full"
-                      label={point.positive ? "For" : "Against"}
-                      size="lg"
-                      variant={point.positive ? "secondary" : "accent"}
-                    />
-                    <span className="inline-block text-primary-900 hover:text-primary-900/80">
-                      {point.title}
-                    </span>
-                  </div>
-                </AccordionTrigger>
-                <AccordionContent>
-                  <div className="grid grid-cols-1 border-t-[0.5px] border-primary-600 pt-6 mt-2 2xl:pt-12">
-                    {point.point_analysises.map((point_analysis) => (
-                      <div className="mb-8">
-                        {point_analysis.analysis.event.title}
-                      </div>
-                    ))}
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
-
-          {/* <div className="flex flex-col p-4 mb-8" key={point.id}>
-              <h1 className="font-semibold text-2xl">{point.title}</h1>
-              <p className="text-lg">{point.body}</p>
-              <div className="flex flex-col py-4">
-                {point.point_analysises.map((analysis) => (
-                  <div className="flex flex-col">
-                    <div>
-                      <p className="text-purple-800">
-                        {analysis.analysis.content}
-                      </p>
-                      <p className="text-blue">
-                        event: {analysis.analysis.event.title}
-                      </p>
+                  <AccordionTrigger
+                    chevronClassName="h-6 w-6 stroke-[2.5] ml-4"
+                    className="text-lg lg:text-xl 2xl:text-2xl text-primary font-medium text-start hover:no-underline"
+                  >
+                    <div className="flex flex-col">
+                      <Chip
+                        // TODO @seeleng: my tag not centered pls fix thanku :clown:
+                        className="inline-block mb-4 w-fit max-w-full"
+                        label={point.positive ? "For" : "Against"}
+                        size="lg"
+                        variant={point.positive ? "secondary" : "accent"}
+                      />
+                      <span className="inline-block text-primary-900 hover:text-primary-900/80">
+                        {point.title}
+                      </span>
                     </div>
-                    <p className="text-red-500">{analysis.elaboration}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))} */}
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="border-t-[0.5px] border-primary-600 mt-2 pt-4 2xl:pt-8">
+                      <h2 className="text-lg 2xl:text-2xl font-medium">
+                        Examples
+                      </h2>
+
+                      {pointHasAnalysis ? (
+                        <Accordion
+                          className=""
+                          type="multiple"
+                          defaultValue={point.point_analysises.map(
+                            (point_analysis) =>
+                              point_analysis.analysis.id.toString(),
+                          )}
+                        >
+                          {/* TODO: get confidence score, sort and bucket */}
+                          {point.point_analysises.map((point_analysis) => {
+                            const { analysis, elaboration } = point_analysis;
+                            const { id: analysisId, event } = analysis;
+                            return (
+                              <AccordionItem
+                                className="py-4"
+                                value={analysisId.toString()}
+                                key={analysisId}
+                              >
+                                <AccordionTrigger className="xl:text-lg 2xl:text-xl 3xl:text-2xl font-medium">
+                                  {event.title}
+                                </AccordionTrigger>
+                                <AccordionContent className="xl:text-base 2xl:text-lg 3xl:text-xl">
+                                  <div>{elaboration}</div>
+                                </AccordionContent>
+                              </AccordionItem>
+                            );
+                          })}
+                        </Accordion>
+                      ) : (
+                        <div>
+                          <h1>
+                            {point.fallback?.general_argument ??
+                              "No relevant analysis found"}
+                          </h1>
+                          <h1>
+                            {point.fallback?.alt_approach ??
+                              "No relevant analysis found"}
+                          </h1>
+                        </div>
+                      )}
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
         </div>
       </div>
     </div>
