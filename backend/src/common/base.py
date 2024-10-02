33631900d4db07b6afe74bcd37e8b797c5036bd5
seeleng <seelengxd@gmail.com>
2024-10-02 21:45:48 +0800

    feat: add created_at, updated_at columns

--- backend/src/common/base.py
+++ backend/src/common/base.py
@@ -1,5 +1,10 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
-    pass
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, server_default=func.now(), onupdate=func.now()
+    )
