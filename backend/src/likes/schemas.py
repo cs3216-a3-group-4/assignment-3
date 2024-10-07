6f1ff1cdab433e78759d73fdf5c9f3fb991f284c
marcus-ny <kyalsin321@gmail.com>
2024-10-07 14:25:49 +0800

    feat: add nullable analysis_id

--- backend/src/likes/schemas.py
+++ backend/src/likes/schemas.py
@@ -4,14 +4,14 @@ from src.likes.models import LikeType
 
 class LikeData(BaseModel):
     point_id: int | None = None
-    analysis_id: int
+    analysis_id: int | None = None
     # 1 for Like, -1 for Dislike
     type: LikeType
 
 
 class LikeDTO(BaseModel):
     point_id: int | None = None
-    analysis_id: int
+    analysis_id: int | None = None
     # 1 for Like, -1 for Dislike
     type: LikeType
     user_id: int
