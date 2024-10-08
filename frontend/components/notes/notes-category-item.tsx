dc9a7a71fb1c7a7952bc7f55711f3a31c3a908db
Wang Haoyang <haoyangw@gmail.com>
2024-10-09 01:00:43 +0800

    Improve category list on Notes page -Implement onClick for category cards that bring user to the category-specific Notes page -Style the category cards

--- frontend/components/notes/notes-category-item.tsx
+++ frontend/components/notes/notes-category-item.tsx
@@ -1,19 +1,26 @@
 import { getIconFor } from "@/types/categories";
+import { useRouter } from "next/navigation";
 
 interface Props {
     title: string;
+    categoryId: number;
 };
 
-const NotesCategoryItem = ({title}: Props) => {
+const NotesCategoryItem = ({title, categoryId}: Props) => {
     const CategoryIcon = getIconFor(title);
+    const router = useRouter();
+
+    const onClickCategory = () => {
+        router.push(`/notes/${categoryId}`);
+    };
 
     return (
-        <div className="flex">
+        <div onClick={onClickCategory} className="flex items-center py-5 lg:flex-row w-full lg:py-3 px-4 md:px-8 xl:px-12 xl:py-5 gap-x-5 border-y-[1px] lg:border-y-[0px] hover:bg-primary-alt-foreground/[2.5%] lg:rounded-md cursor-pointer">
             <CategoryIcon 
                 className="mr-3 flex-shrink-0"
-                size={20}
+                size={30}
                 strokeWidth={1.7}/>
-            <span className="pointer-events-none">{title}</span>
+            <span className="pointer-events-none text-xl">{title}</span>
         </div>
     );
 };
