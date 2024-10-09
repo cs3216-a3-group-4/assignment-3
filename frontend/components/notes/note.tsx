dcfac264d7221afb61e88a054b7166e1f1eb0a4c
Wang Haoyang <haoyangw@gmail.com>
2024-10-10 00:29:06 +0800

    Remove empty className attribute

--- frontend/components/notes/note.tsx
+++ frontend/components/notes/note.tsx
@@ -37,7 +37,7 @@ const Note = ({ data, handleDelete }: Props) => {
                 {parseDate(dateCreated)}
               </span>
             </div>
-            <p className="">{noteContent}</p>
+            <p>{noteContent}</p>
             <Chip
               className="w-fit"
               label={categoryName}
