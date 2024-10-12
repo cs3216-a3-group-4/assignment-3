016a14a6359f6da50597d89d5d84b3b114365c5b
marcus-ny <kyalsin321@gmail.com>
2024-10-12 18:11:27 +0800

    refactor: lm imports to separate file

---
+++ backend/src/lm/lm.py
@@ -0,0 +1,13 @@
+from src.common.constants import LANGCHAIN_API_KEY, OPENAI_API_KEY
+from langchain_openai import ChatOpenAI
+import os
+
+os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+lm_model = ChatOpenAI(model="gpt-4o-mini", temperature=0.3, max_retries=5)
+
+
+lm_model_essay = ChatOpenAI(
+    model="gpt-4o-mini", temperature=0.7, frequency_penalty=0.5, max_retries=5
+)
