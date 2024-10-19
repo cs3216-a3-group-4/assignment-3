79c31b74f05379ae8ff213375b91958c9aba2aa0
Wang Haoyang <haoyangw@gmail.com>
2024-10-19 15:28:39 +0800

    Add new stripe_session table -Stores the mapping from user.id to subscription.id using the relationship from subscription.id to user.id obtained from stripe checkout session response(on frontend) -Will need frontend to send the stripe session object to backend to establish this relationship -Then backend will update subscription table to match subscription.id to user.id(and therefore user.id to stripe's customer_id)

---
+++ backend/src/billing/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import ForeignKey
+from src.common.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class StripeSession(Base):
+    __tablename__ = "stripe_session"
+
+    id: Mapped[str] = mapped_column(primary_key=True)
+    subscription_id: Mapped[str] = mapped_column(ForeignKey("subscription.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
--- backend/src/billing/schemas.py
+++ backend/src/billing/schemas.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class CheckoutRequestData(BaseModel):
     # Stripe price ID for the current checkout session
     price_id: str
+
+class StripeSessionDTO(BaseModel):
+    id: str
+    subscription_id: str
+    user_id: int
