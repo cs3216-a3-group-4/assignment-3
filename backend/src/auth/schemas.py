e573618f2b58eebb04fd7aa76c783f6eee82f346
Wang Haoyang <haoyangw@gmail.com>
2024-10-20 18:30:23 +0800

    Move backref from User to Subscription defs -Have subscription model backref to user instead of the other way round -Add user object to subscription schema instead of the other way round

--- backend/src/auth/schemas.py
+++ backend/src/auth/schemas.py
@@ -1,7 +1,5 @@
-from typing import Optional
 from pydantic import BaseModel, ConfigDict, EmailStr, Field, model_validator
 from src.categories.schemas import CategoryDTO
-from src.subscriptions.schemas import SubscriptionDTO
 
 
 class UserPublic(BaseModel):
@@ -13,7 +11,6 @@ class UserPublic(BaseModel):
     categories: list[CategoryDTO]
     top_events_period: int = 7
     tier_id: int = 1
-    subscription: Optional[SubscriptionDTO] = None
 
 
 class Token(BaseModel):
