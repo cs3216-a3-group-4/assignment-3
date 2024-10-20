bd23123094d623b3866152fd56e10e16add522e9
Wang Haoyang <haoyangw@gmail.com>
2024-10-20 18:08:11 +0800

    Implement important stripe event handlers -Implement important stripe webhook event handlers for events related to basic payment and subscription

--- backend/src/billing/router.py
+++ backend/src/billing/router.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from http import HTTPStatus
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Request, Header
@@ -7,6 +8,7 @@ from src.auth.models import User
 from src.billing.models import StripeSession
 from src.common.constants import FRONTEND_URL, STRIPE_API_KEY, STRIPE_WEBHOOK_SECRET
 from src.common.dependencies import get_session
+from src.subscriptions.models import Subscription
 from .schemas import CheckoutRequestData
 import stripe
 
@@ -55,14 +57,15 @@ async def create_checkout_session(
 
         # Return stripe checkout URL to frontend for redirect
         return RedirectResponse(
-            url=checkout_session.url, status_code=HTTPStatus.SEE_OTHER
+            url=checkout_session.url if checkout_session.url else "", 
+            status_code=HTTPStatus.SEE_OTHER
         )
     except Exception as e:
         raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=str(e))
 
 
 @router.post("/webhook")
-async def stripe_webhook(request: Request, stripe_signature: str = Header(None)):
+async def stripe_webhook(request: Request, stripe_signature: str = Header(None), session=Depends(get_session),):
     payload = await request.body()
     webhook_secret = STRIPE_WEBHOOK_SECRET
 
@@ -76,28 +79,28 @@ async def stripe_webhook(request: Request, stripe_signature: str = Header(None))
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST, detail=f"""Invalid payload: {e}"""
         )
-    except stripe.error.SignatureVerificationError as e:
+    except stripe.error.SignatureVerificationError as e: # type: ignore
         # Invalid signature
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST, detail=f"""Invalid signature: {e}"""
         )
 
     if event_type == "checkout.session.completed":
-        handle_checkout_completed(event)
+        handle_checkout_completed(event, session)
     elif event_type == "invoice.paid":
         handle_payment_success(event)
     elif event_type == "invoice.payment_failed":
         handle_payment_failure(event)
     elif event_type == "customer.subscription.created":
-        handle_subscription_created(event)
+        handle_subscription_created(event, session)
     elif event_type == "customer.subscription.deleted":
-        handle_subscription_canceled(event)
+        handle_subscription_canceled(event, session)
     elif event_type == "customer.subscription.paused":
-        handle_subscription_paused(event)
+        handle_subscription_paused(event, session)
     elif event_type == "customer.subscription.resumed":
-        handle_subscription_resumed(event)
+        handle_subscription_resumed(event, session)
     elif event_type == "customer.subscription.updated":
-        handle_subscription_updated(event)
+        handle_subscription_updated(event, session)
     elif event_type == "charge.dispute.created":
         handle_charge_dispute_created(event)
     elif event_type == "charge.dispute.closed":
@@ -106,9 +109,9 @@ async def stripe_webhook(request: Request, stripe_signature: str = Header(None))
     return {"status": "success"}
 
 
-def handle_checkout_completed(event):
-    # Update your database to reflect checkout completion
-    pass
+def handle_checkout_completed(event, session):
+    checkout_session: stripe.checkout.Session = event.data.object
+    update_subscription_for(checkout_session, session)
 
 
 def handle_payment_success(event):
@@ -121,29 +124,34 @@ def handle_payment_failure(event):
     pass
 
 
-def handle_subscription_created(event):
-    # Mark subscription as active in the database
-    pass
+def handle_subscription_created(event, session):
+    checkout_session: stripe.checkout.Session = event.data.object
+    update_subscription_for(checkout_session, session)
+    # TODO: Update user tier_id
 
 
-def handle_subscription_canceled(event):
-    # Mark subscription as canceled in the database
-    pass
+def handle_subscription_canceled(event, session):
+    checkout_session: stripe.checkout.Session = event.data.object
+    update_subscription_for(checkout_session, session)
+    # TODO: Update user tier_id
 
 
-def handle_subscription_paused(event):
-    # Mark subscription as paused in the database
-    pass
+def handle_subscription_paused(event, session):
+    checkout_session: stripe.checkout.Session = event.data.object
+    update_subscription_for(checkout_session, session)
+    # TODO: Consider how to notify the frontend about this
 
 
-def handle_subscription_resumed(event):
-    # Mark subscription as resumed in the database
-    pass
+def handle_subscription_resumed(event, session):
+    checkout_session: stripe.checkout.Session = event.data.object
+    update_subscription_for(checkout_session, session)
+    # TODO: Consider how to notify the frontend about this
 
 
-def handle_subscription_updated(event):
-    # Update subscription status (e.g., trial ended, renewed)
-    pass
+def handle_subscription_updated(event, session):
+    checkout_session: stripe.checkout.Session = event.data.object
+    update_subscription_for(checkout_session, session)
+    # TODO: Update user tier_id if needed
 
 def handle_charge_dispute_created(event):
     # Update user subscription status to paused
@@ -152,3 +160,36 @@ def handle_charge_dispute_created(event):
 def handle_charge_dispute_closed(event):
     # Update user subscription status based on whether customer won or we won
     pass
+
+def update_subscription_for(checkout_session: stripe.checkout.Session, session):
+    if (checkout_session.mode != 'subscription'):
+        return
+    # Update stripe_session table to with updated subscription data from stripe
+    subscriptionId: str = str(checkout_session.subscription)
+    customer_id: str = str(checkout_session.customer)
+    stripe_session = session.get(StripeSession, checkout_session.id)
+    user_id: int = int(checkout_session.client_reference_id if checkout_session.client_reference_id else stripe_session.user_id)
+    
+    stripe_subscription = stripe.Subscription.retrieve(subscriptionId)
+    new_subscription = Subscription(
+        id=subscriptionId,
+        user_id=user_id,
+        price_id=stripe_subscription.items.data[0].price.id,
+        customer_id=customer_id,
+        status=stripe_subscription.status,
+        quantity=stripe_subscription.items.data[0].quantity,
+        subscription_period_end=datetime.fromtimestamp(stripe_subscription.current_period_end).isoformat() if stripe_subscription.current_period_end else None,
+        subscription_ended_date=datetime.fromtimestamp(stripe_subscription.ended_at).isoformat() if stripe_subscription.ended_at else None,
+        subscription_cancel_at=datetime.fromtimestamp(stripe_subscription.cancel_at_period_end).isoformat() if stripe_subscription.cancel_at_period_end else None,
+        subscription_cancelled_date=datetime.fromtimestamp(stripe_subscription.canceled_at).isoformat() if stripe_subscription.canceled_at else None,
+    )
+    new_subscription.user = session.get(User, new_subscription.user_id)
+    session.add(new_subscription)
+    session.commit()
+    session.refresh(new_subscription)
+
+    if not stripe_session.subscription_id:
+        stripe_session.subscription_id = subscriptionId
+        session.add(stripe_session)
+        session.commit()
+        session.refresh(stripe_session)
