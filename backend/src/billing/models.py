d71e46e7be70e5dc3ce0f96f049335110c828ea8
Wang Haoyang <haoyangw@gmail.com>
2024-10-20 16:05:33 +0800

    Save tier_id with Stripe session -Add tier_id to stripe_session table to save our internal tier_id together with each checkout session

--- backend/src/billing/models.py
+++ backend/src/billing/models.py
@@ -9,3 +9,5 @@ class StripeSession(Base):
     id: Mapped[str] = mapped_column(primary_key=True)
     subscription_id: Mapped[str] = mapped_column(ForeignKey("subscription.id"))
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    # TODO: Update this to be a foreign key to the tier table
+    tier_id: Mapped[int]
