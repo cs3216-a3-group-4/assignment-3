c3ead26e0a0a5fbeb73c2f396d33fade0acd8ad9
Wang Haoyang <haoyangw@gmail.com>
2024-10-20 22:24:59 +0800

    make subscription.user_id nullable again -user.subscription is nullable, so subscription.user_id should be nullable as well

--- backend/src/subscriptions/models.py
+++ backend/src/subscriptions/models.py
@@ -17,7 +17,7 @@ class Subscription(Base):
     __tablename__ = "subscription"
 
     id: Mapped[str] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=True)
     price_id: Mapped[str]
     customer_id: Mapped[str]
     subscription_period_end: Mapped[datetime] = mapped_column(nullable=True)
