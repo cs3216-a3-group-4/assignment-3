873911b3d42b0d83a1a871764052de211b4c1bed
Wang Haoyang <haoyangw@gmail.com>
2024-10-20 16:06:26 +0800

    Pass tier_id with stripe checkout request -Have frontend pass tier_id with stripe checkout request so we can populate stripe_session.tier_id after we create new stripe session

--- backend/src/billing/schemas.py
+++ backend/src/billing/schemas.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 class CheckoutRequestData(BaseModel):
     # Stripe price ID for the current checkout session
     price_id: str
+    tier_id: int
 
 
 class StripeSessionDTO(BaseModel):
