06d62bde4e7326b312b5eb482e64a8ddabe4a4cc
Wang Haoyang <haoyangw@gmail.com>
2024-10-23 15:53:48 +0800

    Update stripe_session table -Make subscription_id foreign key nullable since we have to insert into the table without subscription_id every time user creates a new stripe session

--- backend/src/billing/models.py
+++ backend/src/billing/models.py
@@ -7,7 +7,7 @@ class StripeSession(Base):
     __tablename__ = "stripe_session"
 
     id: Mapped[str] = mapped_column(primary_key=True)
-    subscription_id: Mapped[str] = mapped_column(ForeignKey("subscription.id"))
+    subscription_id: Mapped[str] = mapped_column(ForeignKey("subscription.id"), nullable=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     # TODO: Update this to be a foreign key to the tier table
     tier_id: Mapped[int]
