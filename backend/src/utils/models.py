7b9312e8acd74f64e7a492203cc86c0a04fc41b0
Wang Haoyang <haoyangw@gmail.com>
2024-10-23 15:27:33 +0800

    Import new models into alembic -Forgot to import the new billing/ and subscriptions/ models.py into alembic

--- backend/src/utils/models.py
+++ backend/src/utils/models.py
@@ -7,3 +7,5 @@ from src.notes import models as note_models  # noqa: F401
 from src.likes import models as like_models  # noqa: F401
 from src.limits import models as limit_models  # noqa: F401
 from src.essays import models as essay_models  # noqa: F401
+from src.subscriptions import models as subscription_models  # noqa: F401
+from src.billing import models as billing_models  # noqa: F401
