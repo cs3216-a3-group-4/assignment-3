17845bf84be268d2a339225274b0a61719025678
Wang Haoyang <haoyangw@gmail.com>
2024-10-24 22:42:35 +0800

    Unset stripe_session.subscription_id as foreign key -Stop setting stripe_session.subscription_id as foreign key to allow stripe webhook events to come in arbitray order -This fixes the case where customer.subscription.created webhook event arrives before checkout.session.completed event

--- backend/src/billing/models.py
+++ backend/src/billing/models.py
@@ -7,7 +7,9 @@ class StripeSession(Base):
     __tablename__ = "stripe_session"
 
     id: Mapped[str] = mapped_column(primary_key=True)
-    subscription_id: Mapped[str] = mapped_column(ForeignKey("subscription.id"), nullable=True)
+    # subscription ID will only be available after checkout completes, 
+    #   which happens after creating a session that we save in this table first
+    subscription_id: Mapped[str | None] = mapped_column(nullable=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     # TODO: Update this to be a foreign key to the tier table
     tier_id: Mapped[int]
