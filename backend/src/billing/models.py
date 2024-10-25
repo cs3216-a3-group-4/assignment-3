ae0d4c7e1c0d57ccfcfbfcfa3aff2fffacaf7b91
Wang Haoyang <haoyangw@gmail.com>
2024-10-26 00:46:03 +0800

    Add stripe price and product tables -Add tables to store stripe product and price objects

--- backend/src/billing/models.py
+++ backend/src/billing/models.py
@@ -1,7 +1,12 @@
-from sqlalchemy import ForeignKey
+from enum import Enum
+from sqlalchemy import ARRAY, ForeignKey, String
 from src.common.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
 
+
+class StripePaymentInterval(str, Enum):
+    MONTHLY = "month"
+    ANNUAL = "year"
 
 class StripeSession(Base):
     __tablename__ = "stripe_session"
@@ -13,3 +18,25 @@ class StripeSession(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     # TODO: Update this to be a foreign key to the tier table
     tier_id: Mapped[int]
+
+class StripeProduct(Base):
+    __tablename__ = "stripe_product"
+
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    image_url: Mapped[str]
+    is_active: Mapped[bool]
+    features: Mapped[list[str]] = mapped_column(ARRAY(String))
+
+class StripePrice(Base):
+    __tablename__ = "stripe_price"
+
+    id: Mapped[str] = mapped_column(primary_key=True)
+    product_id: Mapped[str]
+    description: Mapped[str]
+    payment_interval: Mapped[StripePaymentInterval]
+    # Price in cents
+    price: Mapped[int]
+    currency: Mapped[str]
+    is_active: Mapped[bool]
