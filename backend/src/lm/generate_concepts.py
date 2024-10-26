862d5d12e12cdef66911f5b9e51574ef1e33dd0b
marcus-ny <kyalsin321@gmail.com>
2024-10-26 15:05:02 +0800

    fix: trailing if statement

--- backend/src/lm/generate_concepts.py
+++ backend/src/lm/generate_concepts.py
@@ -171,6 +171,7 @@ async def generate_concepts(limit: int | None = None, add_to_db: bool = True):
 
 if __name__ == "__main__":
     # TODO(marcus): probably remove/change this
+    pass
     # asyncio.run(generate_concepts())
     # parser = JsonOutputParser(pydantic_object=ArticleConceptsWithId)
     # res = """{
