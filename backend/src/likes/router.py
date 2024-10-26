2412f274e2c5be87a9f02002c1f9e843cc7a8de5
Chloe Lim <chloelim22@gmail.com>
2024-10-26 19:51:51 +0800

    refactor: clean up upsert_like

--- backend/src/likes/router.py
+++ backend/src/likes/router.py
@@ -20,30 +20,26 @@ def upsert_like(
     user: Annotated[User, Depends(get_current_user)],
     session=Depends(get_session),
 ):
-    # 1. check if the analysis or point or concept exists
-    analysis = session.get(Analysis, data.analysis_id)
-    point = session.get(Point, data.point_id)
-    concept = session.get(Concept, data.concept_id)
-
-    if not analysis or not point or not concept:
-        raise HTTPException(HTTPStatus.NOT_FOUND, "like entity doesn't exist")
-
-    # TODO: temporary workaround- should remove analysis once migration is complete
+    # assumes that for each like row, only 1 of concept_id, analysis_id, or point_id is populated
+    # the implementation is less than ideal and should be refactored (but we probably never will)
+    query = select(Like).where(Like.user_id == user.id)  # only retrieve user's likes
     if data.concept_id:
-        query = select(Like).where(Like.concept_id == data.concept_id)
-    else:
-        query = select(Like).where(Like.analysis_id == data.analysis_id)
-
-    if data.point_id:
+        concept = session.get(Concept, data.concept_id)
+        if not concept:
+            raise HTTPException(HTTPStatus.NOT_FOUND, "concept doesn't exist")
+        query = query.where(Like.concept_id == data.concept_id)
+    elif data.analysis_id:
+        analysis = session.get(Analysis, data.analysis_id)
+        if not analysis:
+            raise HTTPException(HTTPStatus.NOT_FOUND, "analysis doesn't exist")
+        query = query.where(Like.analysis_id == data.analysis_id)
+    elif data.point_id:
         point = session.get(Point, data.point_id)
         if not point:
             raise HTTPException(HTTPStatus.NOT_FOUND, "point doesn't exist")
-        query = query.where(Like.point_id == data.point_id)
+        query = query.where(Like.analysis_id == data.analysis_id)
     else:
-        # TODO: assumes concept like has no point_id, might have to update
-        query = query.where(Like.point_id.is_(None))
-
-    query = query.where(Like.user_id == user.id)
+        raise HTTPException(HTTPStatus.NOT_FOUND, "point entity doesn't exist")
 
     # 2. check if the like exists
     like = session.scalar(query)
