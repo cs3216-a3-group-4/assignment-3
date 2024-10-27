881d7d98ac538e6f0e76ba5d8e8e61fc07016816
Chloe Lim <chloelim22@gmail.com>
2024-10-28 03:06:10 +0800

    fix: homescreen ui touchups

--- frontend/components/news/news-article.tsx
+++ frontend/components/news/news-article.tsx
@@ -34,19 +34,19 @@ const NewsArticle = (props: { newsArticle: MiniArticleDTO }) => {
 
   return (
     <div
-      className="flex h-full flex-col py-4 w-full lg:py-6 px-4 md:px-8 xl:px-12 xl:py-10 gap-x-28 border-y-[1px] lg:border-y-[0px] hover:bg-primary-alt-foreground/[2.5%] lg:rounded-md cursor-pointer"
+      className="flex h-full flex-col py-4 w-full lg:py-6 sm:px-4 md:px-8 xl:px-12 xl:py-10 gap-x-28 border-t-[1px] first:border-none lg:border-y-[0px] hover:bg-primary-alt-foreground/[2.5%] lg:rounded-md cursor-pointer"
       onClick={onClick}
     >
       <div className="hidden sm:flex w-full justify-between text-text-muted/90 mt-2">
         <span>{articleSourceToDisplayNameMap[newsArticle.source]}</span>
         <span>{parseDate(newsArticle.date)}</span>
       </div>
-      <div className="flex gap-4 items-stretch">
+      <div className="flex gap-12 items-stretch">
         <div className="flex flex-col w-full lg:w-8/12 2xl:w-9/12 3xl:w-10/12 grow">
-          <h2 className="text-md sm:text-2xl font-semibold mt-2 mb-3 text-primary-900">
+          <h2 className="text-base md:text-lg lg:text-2xl font-semibold mt-2 mb-3 text-primary-800">
             {newsArticle.title}
           </h2>
-          <p className="text-sm sm:text-md line-clamp-3">
+          <p className="text-sm sm:text-base line-clamp-3 text-text">
             {newsArticle.summary}
           </p>
         </div>
@@ -76,14 +76,14 @@ const NewsArticle = (props: { newsArticle: MiniArticleDTO }) => {
           /> */}
         </div>
       </div>
-      <div className="hidden sm:flex flex-wrap gap-x-2 gap-y-2 mt-4 place-end">
+      <div className="hidden sm:flex flex-wrap gap-x-2 gap-y-2 mt-6 -mx-3 place-end">
         {categories?.map((category: Category, index: number) => (
           <Chip
             Icon={categoriesToIconsMap[category]}
+            className="text-sm"
             key={index}
             label={categoriesToDisplayName[category]}
-            size="lg"
-            variant="primary"
+            variant="nobg"
           />
         ))}
       </div>
@@ -97,7 +97,7 @@ const NewsArticle = (props: { newsArticle: MiniArticleDTO }) => {
             key={index}
             label={categoriesToDisplayName[category]}
             size="sm"
-            variant="primary"
+            variant="nobg"
           />
         ))}
       </div>
