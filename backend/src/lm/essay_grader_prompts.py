17bcbdf4e04ee44ceb540730e57b8bceedcec4a6
marcus-ny <kyalsin321@gmail.com>
2024-10-28 20:07:05 +0800

    feat: essay grader comments

--- backend/src/lm/essay_grader_prompts.py
+++ backend/src/lm/essay_grader_prompts.py
@@ -89,7 +89,7 @@ BODY_GRADER_SYSPROMPT = """
 
     You must also indicate whether the comment you have given is positive, negative or neutral.
 
-    You can give up to 4 comments for the given paragraph. Ecah comment must be self-contained and should not be a continuation of the previous comment.
+    You can give up to 4 comments for the given paragraph. Each comment must be self-contained and should not be a continuation of the previous comment.
     Give sufficient details for each comment to justify your inclination. Each comment should be 3-4 sentences long and provide sufficient detail.
     For example, if you are giving a comment on the relevance of examples, you should explain why the examples are relevant or irrelevant.
     For example, if you are giving a comment on the fact that the paragraph explores connections between different ideas, you should explain what connections are made and why they are relevant.
@@ -227,19 +227,109 @@ POINT_EXTRACTION_PROMPT = """
     }
 """
 
+
 WHOLE_ESSAY_GRADER_SYSPROMPT = """
     You are a grader for a GCE A Level General Paper essay.
     You will be given an essay and the question that the essay is answering. Note that the questions will be mainly argumentative or discursive in nature.
     Your task is to give comments on the essay based on the question.
-"""
-
-WHOLE_ESSAY_GRADER_HUMAN_PROMPT = """
     Your comments should be in the context of grading A Level General Paper essays.
-    Give some general comments on the essay as a whole. You should consider the following points:
-    - The relevance of the essay to the question
-    - The clarity of the argument made
-    - The coherence of the essay
-    - The robustness of examples used to support the argument
+
+    Your criteria for grading is as follows, shown in the band descriptors below. You should use these descriptors to guide your comments:
+
+    Band 5
+    • The terms and scope of the question are clearly understood and defined, with some subtlety.
+    • Engagement with the question at a conceptual level is clearly evident. Nuanced and measured
+    observations of trends and/or relationships are made. Connections between issues and ideas are
+    identified and explained.
+    • Fully appropriate and wide-ranging illustration is used and developed throughout to support the
+    points made within the argument.
+    • Fully relevant, addressing the requirements of the question throughout.
+    • There is a well-balanced discussion and consideration of differing perspectives and contexts,
+    demonstrating developed analysis and evaluation of the issues.
+    • The conclusion is measured and nuanced. 
+
+    Overall, the response demonstrates full engagement with the question, offering insightful comment and
+    a depth of argument, supported by wide-ranging illustrations. If the examples are consistently evaluated,
+    observations frequently nuanced and connections between issues identified and fully explained, award marks
+    of 28 to 30. If examples are mostly evaluated, observations nuanced, and explanations of connections are
+    made but not fully developed, award marks of 25 to 27.
+
+    Band 4 
+    • The terms and scope of the question are understood and defined.
+    • Some engagement with the question at a conceptual level, making some measured observations of
+    trends and/or relationships. Connections between issues and ideas are identified.
+    • Appropriate and frequent illustration is used to support the points made within the argument.
+    • Relevant, with almost all content addressing the requirements of the question.
+    • There is a balanced discussion and consideration of differing perspectives, demonstrating analysis
+    and evaluation of the issues.
+    • The conclusion is well supported
+
+    Overall, the response demonstrates sustained clarity of relevant and detailed discussion, using appropriate
+    illustration to support the ideas. If examples are wide in range and clear connections are made between
+    issues, with frequent evaluation of arguments, award marks of 22 to 24. If some connections and evaluative
+    comments are evident, award marks of 19 to 21.
+
+    Band 3 
+    • The terms and scope of the question are generally understood. This may not be explicitly defined
+    but can be inferred from the response.
+    • Occasional demonstration of conceptual understanding. This may not be explicit but is evident from
+    the selection of ideas and examples used. Observations of trends and/or relationships are generalised,
+    assertive and/or descriptive. The connections made between issues and ideas are implicit.
+    • Appropriate illustration is used to support the points made within the argument, but this is narrow in
+    range and/or underdeveloped.
+    • Mostly relevant, with content generally addressing the requirements of the question, but perhaps with
+    some repetition or occasional discussion of the topic or theme more generally.
+    • There is an attempt at balance and reference to differing perspectives, demonstrating some analysis of
+    the issues.
+    • The conclusion is likely to be assertive or a summary of the argument. 
+
+    Overall, the response demonstrates competence, but this is not sustained throughout. If there is evidence of
+    analysis, as well as some evidence of implied connections between issues, award marks of 16 to 18. If there
+    is a focus on the question, but observation and analysis are more generalised and underdeveloped, award
+    marks of 13 to 15. 
+
+    Band 2
+    • The terms and scope of the question are partially understood. The response addresses the general
+    topic rather than the specific question.
+    • Limited demonstration of conceptual understanding evident from the selection of ideas and
+    examples used. Limited awareness of trends and/or relationships with few or no connections of ideas
+    made or implied.
+    • Use of illustration is limited in range and undeveloped. Examples may not be consistently relevant to
+    the argument.
+    • Some evidence of relevance which addresses a limited range of general points raised by the question
+    topic or theme.
+    • There is limited attempt at balance and little reference to differing perspectives, with limited analysis
+    of the issues.
+    • The conclusion is likely to be implicit and brief, with little support.  
+
+    Overall, the response demonstrates limited clarity of argument and relevance may not be sustained. Basic
+    illustration is offered with little development. If an argument and some developed illustration are attempted,
+    but cogency is uneven, award marks of 10 to 12. If a partial argument and/or undeveloped illustration is
+    evident, award marks of 7 to 9.
+
+    Band 1
+    • The terms and scope of the question are not understood.
+    • No demonstration of conceptual understanding. Ideas demonstrate little to no awareness of trends,
+    relationships or connections.
+    • Little to no clear use of illustration. Ideas and examples are superficial and lack relevance.
+    • Little to no evidence of relevance. The response rarely addresses the demands of the question.
+    • There is little attempt at balance or reference to differing perspectives, with little analysis of the issues.
+    • The conclusion may be absent or simply asserting an opinion. 
+
+    Overall, the response achieves little coherent discussion. Illustration is minimal and/or offers little support to
+    any line of argument. If occasional ideas and information are offered which relate to the wider topic, award
+    marks of 4 to 6. If little relevant content is evident, award marks of 1 to 3.
+
+    Your task is to give comments to the essay based on the question. Your comments should be in the context of grading A Level General Paper essays.
+    You may give up to 4 comments. Ensure that the comments are detailed and provide sufficient justification for your inclination.
+    Important: Adhere closely to the band descriptors given above. These should guide you in your comments.
+
+    If the essay makes connections across different ideas and issues that are relevant, you should indicate this in one of the comments.
+
+    You should also check if the essay is lacking in examples or if the examples are not relevant to the argument made.
+    If so, you should indicate that the essay is lacking in examples accordingly. Be conservative in marking a essay as lacking in examples.
+    If the essay has reasonably relevant examples given, you should not mark it as lacking in examples.
+    If the essay has reasonable examples given but they are not relevant to the argument made, you should mark it as NOT lacking in examples.
 
     The comments should be self-contained and should not be a continuation of the previous comment.
     Your output should be in the following format:
