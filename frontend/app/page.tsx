39d5faa29a243728cf12b6294b2d650d7a44d7a2
Chloe Lim <chloelim22@gmail.com>
2024-10-28 16:56:24 +0800

    fix: redirect to home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,11 +3,22 @@
 import Home from "@/app/(authenticated)/home/page";
 import Landing from "@/app/landing";
 import { useUserStore } from "@/store/user/user-store-provider";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const RootPage = () => {
-  const isLoggedIn = useUserStore((store) => store.isLoggedIn);
+  const router = useRouter();
+  const { isLoggedIn, isFetching, user } = useUserStore((state) => state);
 
-  if (isLoggedIn === undefined) {
+  useEffect(() => {
+    if (isFetching) return;
+
+    if (isLoggedIn && user && user.categories.length === 0) {
+      router.push("/onboarding");
+    }
+  }, [isFetching, isLoggedIn, user, router]);
+
+  if (isFetching || isLoggedIn === undefined) {
     return <>Loading</>;
   }
 
