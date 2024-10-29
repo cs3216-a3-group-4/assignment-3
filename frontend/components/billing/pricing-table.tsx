2f214e1f9fa20bbfa08c8fc0fae941fbba2fcbb7
Wang Haoyang <haoyangw@gmail.com>
2024-10-30 05:12:41 +0800

    Fix billing page layout again -Also fix height, width and padding of components in the 'Our Tiers' section of the billing page

--- frontend/components/billing/pricing-table.tsx
+++ frontend/components/billing/pricing-table.tsx
@@ -8,13 +8,13 @@ interface PricingTiers {
 
 const PricingTable = ({ tiers }: PricingTiers) => {
   return (
-    <div className="flex flex-col md:flex-row px-1 py-2 gap-6 items-start w-fit md:w-full">
+    <div className="flex flex-col md:flex-row md:flex-wrap px-1 py-2 gap-6 justify-items-start items-stretch w-fit md:w-full">
       {tiers?.length > 0 ? (
         tiers.map((tier, index) => {
           return (
             <PricingTier
               buttonText={tier.buttonText}
-              className="flex-1 min-w-80 md:min-w-64 w-full md:w-auto"
+              className="flex-1 min-w-80 md:min-w-64 w-full md:w-auto md:h-auto"
               isButtonDisabled={tier.isButtonDisabled}
               key={index}
               onClickBuy={tier.onClickBuy}
