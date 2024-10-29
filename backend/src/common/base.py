1b582657219e3c6fb5cd7bf95da61683d6578518
seeleng <seelengxd@gmail.com>
2024-10-30 01:14:20 +0800

    feat: add deleted_at datetime column

--- backend/src/common/base.py
+++ backend/src/common/base.py
@@ -8,3 +8,4 @@ class Base(DeclarativeBase):
     updated_at: Mapped[datetime] = mapped_column(
         DateTime, server_default=func.now(), onupdate=func.now()
     )
+    deleted_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
