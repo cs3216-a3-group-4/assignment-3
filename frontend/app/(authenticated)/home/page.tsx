054236b6a2524b67a3e4c37a2a48b9fabe2de560
Chloe Lim <chloelim22@gmail.com>
2024-10-30 02:18:13 +0800

    feat: add mobile action bar

--- frontend/app/(authenticated)/home/page.tsx
+++ frontend/app/(authenticated)/home/page.tsx
@@ -3,18 +3,20 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ActionBar from "./action-bar";
 import ArticleFeed from "./article-feed";
 import TopArticleList from "./top-article-list";
+import ActionBarMobile from "./action-bar-mobile";
 
 const Home = () => {
   return (
     <div className="w-full p-4 sm:p-8 bg-muted overflow-y-auto">
+      <ActionBarMobile />
       <ActionBar />
       <div className="flex w-full my-8">
         <Tabs className="w-full" defaultValue="week">
-          <TabsList className="flex w-full font-medium h-12 bg-primary-100/80 text-primary-700">
-            <TabsTrigger className="w-full text-lg" value="week">
+          <TabsList className="flex w-full font-medium md:h-12 bg-primary-100/80 text-primary-700">
+            <TabsTrigger className="w-full md:text-base" value="week">
               Weekly picks
             </TabsTrigger>
-            <TabsTrigger className="w-full text-base" value="yours">
+            <TabsTrigger className="w-full md:text-base" value="yours">
               Today&apos;s news
             </TabsTrigger>
           </TabsList>
