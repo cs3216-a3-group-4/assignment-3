6ea8e6b4f2309d0436a77da60ea441f0281f5e35
Wang Haoyang <haoyangw@gmail.com>
2024-10-31 15:46:32 +0800

    Improve verify email page -Modify the frontend messages on the verify email to sound nice -Update redirect now component to be a span that performs an onClick() function instead of just redirecting to home -Improve redirect after successful verification to selectively redirect to previous or home page depending on user's browser history -Potentially fix multiple runs of the verify backend call from the frontend(pending testing)

--- frontend/app/(authenticated)/verify-email/page.tsx
+++ frontend/app/(authenticated)/verify-email/page.tsx
@@ -4,7 +4,6 @@ import { useEffect, useRef, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { CheckCircleIcon } from "lucide-react";
 
-import { completeEmailVerificationAuthVerifyEmailPut } from "@/client";
 import Link from "@/components/navigation/link";
 import { Box } from "@/components/ui/box";
 import {
@@ -17,41 +16,54 @@ import {
 import { LoadingSpinner } from "@/components/ui/loading-spinner";
 import { useUserStore } from "@/store/user/user-store-provider";
 
+export const UNVERIFIED_TIER_ID = 4;
+export const VERIFY_SUCCESS_DELAY = 1;
+
 export default function VerifyEmail() {
+  const user = useUserStore((store) => store.user);
   const router = useRouter();
-  const setLoggedIn = useUserStore((state) => state.setLoggedIn);
-  const sentAuthentication = useRef(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
 
+  const redirectAfterVerify = () => {
+    if (window.history?.length && window.history.length > 1) {
+      // Redirect the user to their previously accessed page after successful verification
+      router.back();
+    } else {
+      // User has no previous page in browser history, redirect user to Jippy home page
+      router.replace("/", { scroll: false });
+    }
+  };
+
   useEffect(() => {
-    if (!sentAuthentication.current) {
-      sentAuthentication.current = true;
+    const timeout = null;
+    if (code && user?.tier_id === UNVERIFIED_TIER_ID) {
       (async () => {
-        if (!code) {
-          router.push("/login");
-          return;
-        }
-        const response = await completeEmailVerificationAuthVerifyEmailPut({
+        /* const response = await completeEmailVerificationAuthVerifyEmailPut({
           query: { code },
         });
         // There is some problem where this function runs twice, causing an error
         // on the second run since the email verification is used.
         if (response.data) {
           setIsLoading(false);
-          setLoggedIn(response.data!.user);
-          router.push("/");
-        }
+          const timeout = setTimeout(redirectAfterVerify, VERIFY_SUCCESS_DELAY * 1000);
+        } */
       })();
     }
-  }, [code, router, setLoggedIn]);
+    if (timeout) {
+      // Cleanup redirect timeout on unmount of the page
+      return () => clearTimeout(timeout);
+    }
+  }, [code, user]);
 
   return (
-    <Box className="flex flex-col m-auto w-full justify-center items-center gap-y-10">
-      <Card className="flex flex-col border-0 md:border px-6 sm:px-12 sm:py-6 shadow-none md:shadow-sm md:max-w-md text-center">
+    <Box className="flex flex-col m-auto w-full h-full justify-center items-center gap-y-10">
+      <Card className="flex flex-col border-0 md:border shadow-none md:shadow-sm md:max-w-md text-center">
         <CardHeader className="space-y-3">
-          <CardTitle>Verifying your email</CardTitle>
+          <CardTitle>
+            {isLoading ? "Verifying your email" : "Verified! Logging you in"}
+          </CardTitle>
         </CardHeader>
 
         <CardContent className="flex flex-col space-y-8">
@@ -66,12 +78,15 @@ export default function VerifyEmail() {
             <span className="max-w-sm">
               {isLoading
                 ? "Hang tight! We're logging you in. This shouldn't take too long."
-                : "All done! You should be redirected soon."}
+                : "All done! You'll be redirected soon. "}
             </span>
             {!isLoading && (
-              <Link className="p-2" href={"/"} size={"sm"}>
-                Redirect now
-              </Link>
+                <span
+                  className="underline cursor-pointer"
+                  onClick={redirectAfterVerify}
+                >
+                  Redirect now
+                </span>
             )}
           </CardDescription>
         </CardContent>
