6096f3d1347be109774d46972e62d66ac17e6b2a
marcus-ny <kyalsin321@gmail.com>
2024-11-05 04:49:14 +0800

    feat: add answer processing for concept based gen

--- backend/src/essay_helper/form_answer.py
+++ backend/src/essay_helper/form_answer.py
@@ -1,8 +1,51 @@
-from src.user_questions.models import Answer, Fallback, Point, PointAnalysis
-from src.lm.dict_types import PointsAfterLLMType
+from src.user_questions.models import (
+    Answer,
+    Fallback,
+    Point,
+    PointAnalysis,
+    PointArticleConcept,
+)
+from src.lm.dict_types import PointsAfterLLMType, PointsWithConceptsAfterLLMType
 
 
-def form_answer_analysis_based(answer: Answer, results: PointsAfterLLMType) -> Answer:
+def form_answer_concept_based(results: PointsWithConceptsAfterLLMType) -> Answer:
+    """
+    Given an answer and the results of the LLM, returns an Answer data type
+    """
+    answer = Answer()
+    for row in results["for_points"]:
+        point = row["point"]
+        concepts = row["concepts"]
+        point = Point(title=point, body="", positive=True)
+
+        for concept in concepts:
+            point.point_article_concepts.append(
+                PointArticleConcept(
+                    concept_id=concept["concept_id"],
+                    article_id=concept["article_id"],
+                    elaboration=concept["elaborations"],
+                )
+            )
+        answer.points.append(point)
+
+    for row in results["against_points"]:
+        point = row["point"]
+        concepts = row["concepts"]
+        point = Point(title=point, body="", positive=False)
+        for concept in concepts:
+            point.point_article_concepts.append(
+                PointArticleConcept(
+                    concept_id=concept["concept_id"],
+                    article_id=concept["article_id"],
+                    elaboration=concept["elaborations"],
+                )
+            )
+        answer.points.append(point)
+
+    return answer
+
+
+def form_answer_analysis_based(results: PointsAfterLLMType) -> Answer:
     """
     Given an answer and the results of the LLM, returns an Answer data type
     """
