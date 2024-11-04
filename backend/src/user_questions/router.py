2ea52d0312deefe7035fc431a9e9a372d2002970
marcus-ny <kyalsin321@gmail.com>
2024-11-05 04:59:37 +0800

    feat: add concept-based qn gen endpoint

--- backend/src/user_questions/router.py
+++ backend/src/user_questions/router.py
@@ -6,18 +6,24 @@ from sqlalchemy.orm import with_polymorphic, selectinload
 from src.auth.dependencies import get_current_user
 from src.auth.models import User
 from src.common.dependencies import get_session
-from src.events.models import Analysis, Event
+from src.essay_helper.generate_concept_response import generate_concept_response
+from src.events.models import Analysis, ArticleConcept, Event
 from src.notes.models import Note
 from src.limits.check_usage import within_usage_limit
 from src.user_questions.models import (
     Answer,
     Point,
     PointAnalysis,
+    PointArticleConcept,
     UserQuestion,
 )
-from src.essay_helper.form_answer import form_answer_analysis_based
+from src.essay_helper.form_answer import (
+    form_answer_analysis_based,
+    form_answer_concept_based,
+)
 from src.user_questions.schemas import (
     CreateUserQuestion,
+    UserQuestionConceptDTO,
     UserQuestionDTO,
     UserQuestionMiniDTO,
     ValidationResult,
@@ -135,6 +141,54 @@ async def create_user_question(
                 Point.point_analysises,
                 PointAnalysis.analysis,
                 Analysis.category,
+            ),
+        )
+    )
+
+    return same_user_question
+
+
+# Endpoints to create concept-based user questions
+@router.get("/{id}")
+def get_concept_based_user_qn():
+
+@router.post("/")
+async def create_concept_based_user_qn(
+    data: CreateUserQuestion,
+    user: Annotated[User, Depends(get_current_user)],
+    session=Depends(get_session),
+) -> UserQuestionConceptDTO | ValidationResult:
+    validation = within_usage_limit(user, session, data.question)
+
+    if not validation.is_valid:
+        return validation
+
+    user_question = UserQuestion(question=data.question, user_id=user.id)
+
+    results = await generate_concept_response(data.question)
+    answer = form_answer_concept_based(results)
+
+    user_question.answer = answer
+
+    session.add(user_question)
+    session.commit()
+    session.refresh(user_question)
+    same_user_question = session.scalar(
+        select(UserQuestion)
+        .where(UserQuestion.id == user_question.id)
+        .options(
+            selectinload(
+                UserQuestion.answer,
+                Answer.points,
+                Point.point_article_concepts,
+                PointArticleConcept.article_concept,
+                ArticleConcept.article,
+                ArticleConcept.concept,
+            ),
+            selectinload(
+                UserQuestion.answer,
+                Answer.points,
+                Point.fallback,
             ),
         )
     )
