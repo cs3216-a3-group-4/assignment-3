8c30f352e5e1b7d02fdc51d98f4238f2f9d16d07
seeleng <seelengxd@gmail.com>
2024-11-09 20:56:09 +0800

    fix: 🤡

--- frontend/components/auth/user-profile-button.tsx
+++ frontend/components/auth/user-profile-button.tsx
@@ -59,9 +59,7 @@ const UserProfileButton = () => {
                   alt={email}
                   className="rounded-full shadow-inner border-primary/80 object-cover"
                   referrerPolicy="no-referrer"
-                  src={
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCy16nhIbV3pI1qLYHMJKwbH2458oiC9EmA&s"
-                  }
+                  src={image_url}
                 />
               )}
               <AvatarFallback>{getInitialFromEmail(email)}</AvatarFallback>
