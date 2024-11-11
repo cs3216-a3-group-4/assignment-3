c5ffb571e0b409e1d0259b0ee719ec3effeb56e6
Wang Haoyang <haoyangw@gmail.com>
2024-11-11 10:34:45 +0800

    Add new Highlight component -Highlights the text within <Highlight> tags with Jippy Green(and text turns white) for emphasis

---
+++ frontend/components/ui/highlight.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from "react";
+
+interface HighlightProps {
+  children: ReactNode;
+  color?: string;
+}
+
+const Highlight: FC<HighlightProps> = ({ children, color = "bg-primary" }) => {
+  return (
+    <span className={`${color} px-1 rounded-md inline align-baseline text-white`} >
+      {children}
+    </span>
+  );
+};
+
+export default Highlight;
