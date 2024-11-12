4020d5056e8db28a9fba8e480ada07c5b0f24a6b
Wang Haoyang <haoyangw@gmail.com>
2024-11-12 17:18:00 +0800

    Add new FaqAnswer component -Displays 1 Question-Answer part to put in the FAQ of the landing page

---
+++ frontend/components/landing/faq-answer.tsx
@@ -0,0 +1,22 @@
+import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { FaqAnswerDetails } from "@/types/landing";
+
+const FaqAnswer = ({ id, title, description }: FaqAnswerDetails ) => {
+    return (
+        <AccordionItem
+            className="border rounded-lg px-8 py-2 bg-background text-text"
+            value={`q${id}`}
+        >
+            <AccordionTrigger className="text-left">
+                <h3 className="font-medium mr-2">
+                    {title}
+                </h3>
+            </AccordionTrigger>
+            <AccordionContent className="font-medium text-sm">
+                {description}
+            </AccordionContent>
+        </AccordionItem>
+    );
+};
+
+export default FaqAnswer;
