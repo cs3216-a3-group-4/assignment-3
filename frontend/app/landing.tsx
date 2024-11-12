1a953c87575fade560dbe5c1da9adecec50abe30
Wang Haoyang <haoyangw@gmail.com>
2024-11-12 17:23:05 +0800

    Improve landing page -Split features section into more distinct 'Why Jippy' and 'Features' sections -Add highlighting of specific keywords for emphasis and coolness -Add demo of key features in the new 'Features' section with the new FeatureList component -Add more FAQs and corresponding answers -Refactor some components to use .map() so as to reduce repetitive css class declaration and ease maintenance while improving code readability

--- frontend/app/landing.tsx
+++ frontend/app/landing.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
-import { AccordionContent } from "@radix-ui/react-accordion";
-import { Wand2 } from "lucide-react";
-
-import PricingTier from "@/components/billing/pricing-tier";
-import { NAVBAR_HEIGHT } from "@/components/layout/app-layout";
 import {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+  BlocksIcon,
+  BookOpenCheckIcon,
+  NewspaperIcon,
+  Wand2,
+} from "lucide-react";
+
+import { MiniArticleDTO } from "@/client";
+import PricingTier from "@/components/billing/pricing-tier";
+import FaqAnswer from "@/components/landing/faq-answer";
+import FeatureList from "@/components/landing/feature-list";
+import SampleFeedback from "@/components/landing/sample-feedback";
+import SamplePoints from "@/components/landing/sample-points";
+import { NAVBAR_HEIGHT } from "@/components/layout/app-layout";
+import NewsArticle from "@/components/news/news-article";
+import { Accordion } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -22,6 +28,7 @@ import {
   CarouselItem,
   CarouselPagination,
 } from "@/components/ui/carousel";
+import Highlight from "@/components/ui/highlight";
 import {
   JippyTierID,
   tierIDToTierDescription,
@@ -29,11 +36,145 @@ import {
   tierIDToTierName,
   TierPrice,
 } from "@/types/billing";
+import { SampleComment, SamplePoint } from "@/types/landing";
 
+const faqAnswers = [
+  {
+    title: "Who is Jippy built for?",
+    description: "Students taking the Singapore GCE A-Level General Paper.",
+  },
+  {
+    title: "Cannot just ChatGPT meh?",
+    description:
+      "Jippy is trained on a dataset with 13k articles and built specially for A Level GP students.",
+  },
+  {
+    title:
+      "What’s the difference between Jippy’s essay point generation and just searching for examples online?",
+    description:
+      "Jippy’s essay point generation goes beyond providing examples. It offers structured arguments and relevant current affairs linked to each point, saving you time on research and helping you build well-rounded, evidence-backed essays. It’s like having a GP tutor guiding you on how to frame your answers effectively.",
+  },
+  {
+    title: "Can Jippy replace the need for a GP tutor?",
+    description:
+      "While Jippy provides invaluable support for GP revision, it is designed to complement your learning, not replace a tutor entirely. Jippy can be a great asset by giving feedback, curating examples, and offering guidance on essay structure, but human feedback and interaction can still be beneficial for deeper learning.",
+  },
+  {
+    title: "Is my data safe with Jippy?",
+    description:
+      "Yes, we take user privacy and data protection very seriously. Jippy only collects essential information, such as your email and selected GP topics, to personalize your experience. We comply with data protection regulations to ensure your information is secure.",
+  },
+  {
+    title: "Can I use Jippy for other subjects besides GP?",
+    description:
+      "Jippy is specifically designed for A-Level General Paper, focusing on essay writing and current affairs analysis that are unique to GP. While some features may be useful for other subjects, the content and tools are tailored to meet GP requirements.",
+  },
+  {
+    title: "Is Jippy suitable for students outside Singapore?",
+    description:
+      "Jippy is designed with the Singapore A-Level GP syllabus in mind, so it is especially useful for students preparing for that exam. However, some features like essay feedback and current affairs analysis may still be valuable to other students looking to improve their critical thinking and writing skills.",
+  },
+];
+
+const whyJippyReasons = [
+  {
+    reason: "Saves time",
+    description:
+      "Learn current affairs faster with summarised event details. Get feedback for your GP essay within a minute.",
+  },
+  {
+    reason: "Powered by AI",
+    description:
+      "Generate suggested topic sentences with relevant examples and analyses for your chosen GP question. Get comments on strengths and weaknesses for your GP essay anytime.",
+  },
+  {
+    reason: "Latest GP syllabus",
+    description:
+      "Build the conceptual analysis skills required by the latest syllabus with Jippy&apos;s suggested conceptual analysis for each news event. Get comments for your essay based on the 2024 Band Descriptors used by SEAB.",
+  },
+];
+
+const sampleArticle1: MiniArticleDTO = {
+  id: 1,
+  title: "Singapore's COVID-19 vaccination rate hits 80%",
+  summary:
+    "Singapore has hit a milestone in its vaccination programme, with 80% of the population now fully vaccinated.",
+  url: "https://www.straitstimes.com/singapore/singapore-covid-19-vaccination-rate-hits-80",
+  source: "CNA",
+  date: "2024-10-12T00:00:00Z",
+  image_url: "https://via.placeholder.com/150",
+  categories: [],
+  bookmarks: [],
+};
+
+const sampleComments: SampleComment[] = [
+  {
+    inclination: "good",
+    content:
+      "The argument presented is clear and nuanced, as it acknowledges both the negative aspects of business influence on politics while also recognizing the necessity of some level of business involvement in governmental affairs. This balanced view is commendable.",
+  },
+  {
+    inclination: "neutral",
+    content:
+      "While the introduction provides a solid overview, it could benefit from a slightly clearer distinction between agreeing with certain aspects and disagreeing with the overall statement. A more explicit articulation of this stance would enhance clarity.",
+  },
+];
+
+const samplePoints: SamplePoint[] = [
+  {
+    positive: true,
+    title:
+      "The value of work can be assessed by the salary it commands because high salaries often reflect the demand and scarcity of skills required for certain jobs, indicating their economic value.",
+  },
+  {
+    positive: false,
+    title:
+      "The value of work cannot be solely assessed by the salary it commands because many essential roles, such as caregiving or teaching, provide significant societal contributions that are not adequately reflected in their pay.",
+  },
+];
+
+const jippyFeatures = [
+  {
+    title: "Curated News",
+    description:
+      "Browse news events for only your selected GP topics. Learn faster with summarised event details and suggested conceptual analyses.",
+    icon: <NewspaperIcon />,
+    featureRender: (
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+        }}
+      >
+        <NewsArticle newsArticle={sampleArticle1} />
+      </div>
+    ),
+  },
+  {
+    title: "AI Essay Feedback",
+    description:
+      "Identify your strengths and get suggestions to improve your submitted essay, getting you closer to the next essay grading Band.",
+    icon: <BlocksIcon />,
+    featureRender: (
+      <>
+        <SampleFeedback comments={sampleComments} />
+      </>
+    ),
+  },
+  {
+    title: "AI Essay Helper",
+    description:
+      "Generate suggested essay points with relevant examples and argumentative analyses for any provided GP question.",
+    icon: <BookOpenCheckIcon />,
+    featureRender: (
+      <>
+        <SamplePoints points={samplePoints} />
+      </>
+    ),
+  },
+];
 
 const Landing = () => {
-
-
   return (
     <div className="relative w-full h-full overflow-y-auto scroll-smooth">
       <div className="flex flex-col bg-muted min-w-full h-fit">
@@ -45,7 +186,7 @@ const Landing = () => {
             Your GP Teacher isn’t the only one who croaks.
           </h1>
           <div>
-            <h2 className="flex max-w-2xl text-center text-xl md:text-2xl text-text-muted/80 animate-jump-in animate-delay-600">
+            <h2 className="max-w-2xl text-center text-xl md:text-2xl text-text-muted/80 animate-jump-in animate-delay-600">
               Jippy is the first AI-powered tool to help you frog-leap your
               General Paper grades.
             </h2>
@@ -67,68 +208,83 @@ const Landing = () => {
           className="flex flex-col items-center justify-center bg-background w-full px-8 py-12 md:px-12 lg:py-24 lg:px-24"
           id="learn-more"
         >
-          <h2 className="flex w-full text-center text-3xl md:text-5xl leading-tight text-primary justify-center font-bold max-w-[50rem] tracking-tight">
-            Feeling overwhelmed with A-Level General Paper studies?
+          <h2 className="w-full text-center text-3xl md:text-5xl leading-tight text-primary justify-center font-bold max-w-[50rem] tracking-tight">
+            Feeling <Highlight>overwhelmed</Highlight> with A-Level General
+            Paper studies?
           </h2>
-          <h3 className="text-xl md:text-2xl lg:text-3xl font-medium mt-3 text-center">
+          <h3 className="text-xl md:text-2xl lg:text-3xl font-medium mt-3 text-center mb-10">
             We&apos;ve been there. Learn how Jippy can help.
           </h3>
           <div className="flex w-full">
-            <Carousel className="w-full" opts={{ align: "center", loop: true }}>
+            <Carousel
+              className="md:hidden w-full"
+              opts={{ align: "center", loop: true }}
+            >
               <CarouselContent className="flex items-stretch mt-8 lg:mt-16">
-                <CarouselItem className="flex flex-col basis-full md:basis-7/12">
-                  <Card className="flex-1 m-1 drop-shadow-sm shadow-background-50 px-4 py-3">
-                    <CardHeader>
-                      <CardTitle className="text-primary-800 text-3xl">
-                        Saves time
-                      </CardTitle>
-                      <CardDescription className="text-xl text-text-muted pt-3">
-                        Learn current affairs faster with summarised event details. Get feedback for your GP essay within a minute.
-                      </CardDescription>
-                    </CardHeader>
-                  </Card>
-                </CarouselItem>
-
-                <CarouselItem className="flex flex-col basis-full md:basis-7/12">
-                  <Card className="flex-1 m-1 drop-shadow-sm shadow-background-50 px-4 py-3">
-                    <CardHeader>
-                      <CardTitle className="text-primary-800 text-3xl">
-                        Powered by AI
-                      </CardTitle>
-                      <CardDescription className="text-xl text-text-muted pt-3">
-                        Generate suggested topic sentences with relevant examples and analyses for your chosen GP question. Get comments on strengths and weaknesses for your GP essay anytime.
-                      </CardDescription>
-                    </CardHeader>
-                  </Card>
-                </CarouselItem>
-
-                <CarouselItem className="flex flex-col basis-full md:basis-7/12">
-                  <Card className="flex-1 m-1 drop-shadow-sm shadow-background-50 px-4 py-3">
-                    <CardHeader>
-                      <CardTitle className="text-primary-800 text-3xl">
-                        Latest GP syllabus
-                      </CardTitle>
-                      <CardDescription className="text-xl text-text-muted pt-3">
-                        Build the conceptual analysis skills required by the latest syllabus with Jippy's suggested conceptual analysis for each news event. Get comments for your essay based on the 2024 Band Descriptors used by SEAB.
-                      </CardDescription>
-                    </CardHeader>
-                  </Card>
-                </CarouselItem>
+                {whyJippyReasons.map((whyJippyReason, index) => (
+                  <CarouselItem
+                    className="flex flex-col basis-full md:basis-7/12"
+                    key={index}
+                  >
+                    <Card className="flex-1 m-1 drop-shadow-sm shadow-background-50 px-4 py-3">
+                      <CardHeader>
+                        <CardTitle className="text-primary-800 text-3xl">
+                          {whyJippyReason.reason}
+                        </CardTitle>
+                        <CardDescription className="text-xl text-text-muted pt-3">
+                          {whyJippyReason.description}
+                        </CardDescription>
+                      </CardHeader>
+                    </Card>
+                  </CarouselItem>
+                ))}
               </CarouselContent>
               <CarouselPagination />
             </Carousel>
+            <div className="hidden md:flex md:flex-row md:flex-wrap w-full h-fit items-stretch justify-start gap-x-5 gap-y-2.5">
+              {whyJippyReasons.map((whyJippyReason, index) => (
+                <Card
+                  className="basis-1/3 flex-1 m-1 drop-shadow-sm shadow-background-50 px-4 py-3 w-full min-w-[300px] md:w-auto md:h-auto"
+                  key={index}
+                >
+                  <CardHeader>
+                    <CardTitle className="text-primary-800 text-3xl">
+                      {whyJippyReason.reason}
+                    </CardTitle>
+                    <CardDescription className="text-xl text-text-muted pt-3">
+                      {whyJippyReason.description}
+                    </CardDescription>
+                  </CardHeader>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-center bg-background w-full px-8 py-12 md:px-12 lg:py-24 lg:px-24">
+          <h2 className="w-full text-center text-3xl md:text-5xl leading-tight text-primary justify-center font-bold max-w-[50rem] tracking-tight">
+            Built for <Highlight>all</Highlight> your GP essay needs
+          </h2>
+          <h3 className="text-sm lg:text-lg font-medium mt-10 text-center w-9/12 mb-10 md:mb-20">
+            Whether you need to build your example bank for your next essay, get
+            comments to improve your practice essay, or generate essay points
+            for a new essay prompt you&apos;re stuck on, Jippy has what you need for
+            your essay revision.
+          </h3>
+          <div className="flex flex-row flex-wrap w-full h-fit items-stretch justify-center">
+            <FeatureList features={jippyFeatures} />
           </div>
         </div>
 
         <div className="flex flex-col md:flex-row bg-background w-full px-8 py-12 md:px-12 lg:py-24 lg:px-24 md:gap-x-24">
-          <div className="flex-1 mb-6 md:basis-5/12">
-            <span className="text-2xl font-medium flex items-center">
-              <Wand2 className="inline-flex mr-3 hover:animate-wiggle-more text-text-muted" />
-              Learn how Jippy AI works
-            </span>
+          <div className="flex flex-col flex-1 mb-6 md:basis-5/12 w-full md:w-fit items-center md:items-start">
             <h1 className="text-5xl tracking-tight leading-tight text-primary font-bold">
               Why you can trust Jippy
             </h1>
+            <span className="mt-3 text-2xl font-medium flex items-center">
+              <Wand2 className="inline-flex mr-3 hover:animate-wiggle-more text-text-muted" />
+              Learn how Jippy AI works
+            </span>
           </div>
 
           <div className="flex-1 md:basis-7/12 p-12 bg-background border rounded-lg shadow-lg">
@@ -171,12 +327,12 @@ const Landing = () => {
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row bg-background w-full py-16 md:py-24 px-8 md:px-24 md:gap-x-24">
-          <div className="flex-1 md:basis-5/12">
-            <h1 className="text-5xl tracking-tight leading-tight text-primary font-bold">
+        <div className="flex flex-col items-center bg-background w-full py-16 md:py-24 px-8 md:px-24">
+          <div className="flex flex-col items-center">
+            <h1 className="text-center max-w-[50rem] text-5xl tracking-tight leading-tight text-primary font-bold">
               Frequently asked questions
             </h1>
-            <div className="text-2xl mt-4">
+            <div className="text-center text-xl mt-4">
               <span>Still curious? Drop us an email at </span>
               <a
                 className="hover:text-text/80"
@@ -187,32 +343,19 @@ const Landing = () => {
             </div>
           </div>
 
-          <div className="flex-1 md:basis-7/12 md:p-12 gap-y-8 mt-8 md:mt-0">
-            <Accordion className="flex flex-col gap-y-8" type="multiple">
-              <AccordionItem
-                className="border rounded-lg px-8 py-2 bg-background text-2xl font-medium text-text"
-                value="q1"
-              >
-                <AccordionTrigger className="text-left">
-                  Who is Jippy built for?
-                </AccordionTrigger>
-                <AccordionContent>
-                  Students taking the Singapore GCE A-Level paper.
-                </AccordionContent>
-              </AccordionItem>
-
-              <AccordionItem
-                className="border rounded-lg px-8 py-2 bg-background text-2xl font-medium text-text"
-                value="q2"
-              >
-                <AccordionTrigger className="text-left">
-                  Cannot just ChatGPT meh?
-                </AccordionTrigger>
-                <AccordionContent>
-                  Jippy is trained on a dataset with 13k articles and built
-                  specially for A Level GP students.
-                </AccordionContent>
-              </AccordionItem>
+          <div className="flex flex-col w-full md:p-12 gap-y-8 mt-8">
+            <Accordion
+              className="flex flex-col gap-y-8 items-stretch w-full max-w-2xl mx-auto"
+              type="multiple"
+            >
+              {faqAnswers.map((faqAnswer, index) => (
+                <FaqAnswer
+                  description={faqAnswer.description}
+                  id={index}
+                  key={index}
+                  title={faqAnswer.title}
+                />
+              ))}
             </Accordion>
           </div>
         </div>
